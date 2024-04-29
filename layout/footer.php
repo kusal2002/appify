fa7380fe931d007803045b02df9428cf2a892128
Kusal Salpura <kusalsalpura@gmail.com>
2024-04-29 12:21:22 +0530

    layout

--- layout/footer.php
+++ layout/footer.php
@@ -1,3 +1,28 @@
-<footer>
-    <h1>Footer</h1>
-</footer>
+<div class="footer">
+    <div class="container">
+        <div class="logo">
+            <img src="img/logo.png" alt="logo">
+        </div>
+        <div class="links">
+            <table>
+                <tr>
+                    <td><h1>Home</h1></td>
+                    <td><h1>Policies</h1></td>
+                    <td><a href="#">Contact</a></td>
+                </tr>
+                <tr>
+                    <td><a href="#">Blog</a></td>
+                    <td><a href="#">Feedback</a></td>
+                    <td><a href="#">Support</a></td>
+                </tr>
+                <tr>
+                    <td><a href="#">Feedback</a></td>
+  <!-- #region ``-->                    <td><a href="#">Support</a></td>
+  <td>helop</td>
+                </tr>
+
+            </table>
+
+        </div>
+    </div>
+</div>
