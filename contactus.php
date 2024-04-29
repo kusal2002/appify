876c8e3aed6188d50f7054ca93b68ae99636b52f
Tudakshana <tudakshanamjayawardhana@gmail.com>
2024-04-30 03:32:03 +0530

    contactus done

---
+++ contactus.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contact Us</title>
+    <link rel="stylesheet" href="./css/contactus.css">
+</head>
+
+<body>
+    <div class="space"></div>
+    <div class="full">
+        
+        <div class="left">
+            
+            <h1 class="head">Contact Us</h1><br>
+            <p class="info">
+                <img src="./Img/locationtag.svg">
+                1777 West Street, Portland, OR 97205
+                <br><br>
+                <img src="./Img/telephone.svg">
+                (+1) 123 456 7893
+                <br><br>
+                <img src="./Img/mail.svg">
+                name@email.com
+
+            </p>
+            <img src="./Img/image.jpeg" class="image">
+
+        </div>
+        <div class="line"></div>
+        <div class="right">
+            <form action="" class="form">
+                <input type="text" placeholder="Your Name" class="textbox"><br><br>
+                <input type="text" placeholder="Your Email" class="textbox"><br><br>
+                <input type="text" placeholder="your Phone Number" class="textbox"><br><br>
+                <textarea class="area" id="" cols="30" rows="10" placeholder="Your Message"></textarea> <br><br>
+                <input type="submit" class="button" value="Send Message">
+            </form>
+
+        </div>
+
+
+</body>
+
+</html>
