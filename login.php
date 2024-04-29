865ab4b34daf096e589f34edfd96b912f191df0b
Chamathka Rathnayake <chamathkanrathnayake2002@gmail.com>
2024-04-29 21:35:43 +0530

    login,signup,faq
    created log in , sign up pages and faq ongoing

--- login.php
+++ login.php
@@ -7,15 +7,22 @@
 
     <style media="screen">
 
-    </style>
+    </style>  
   </head>
   <body>
     <form id="login" action="login_check.php" method="post">
-      <label for="username">Usernameeee</label>
-      <input type="text" id="username" name="username"><br>
-      <label for="password">Password</label>
-      <input type="password" id="password" name="password"><br>
-      <button type="submit" name="login">Login</button>
+      <h1><center>Login</h1><br>
+      <center><button type="submit">User&nbsp;</button>
+    <button type="submit">Publisher</button></center><br><br>
+    <lable >Email</lable>
+    <input type="text" id="email" name="email">
+    <lable>Password</lable>
+    <input type="text" id="password" name="password"><br><br>
+    
+    <p><input type="checkbox" id="rememberme" name="rememberme">
+     Remember me &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href="kofie">Forgot password?</a></p><br>
+    <center><button type="submit" name="register">LOGIN</button><center>
+    <h5><center>Don't have an account?&nbsp;<a href="kofie">sign up</a></h5>
     </form>
   </body>
 </html>
