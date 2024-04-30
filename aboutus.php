8d162d0d6da6904e9dc083e8ac14a272db3a5012
Tudakshana <tudakshanamjayawardhana@gmail.com>
2024-04-30 17:35:26 +0530

    iconfolder

---
+++ aboutus.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>About US</title>
+    <link rel="stylesheet" href="./css/about_style.css">       
+</head>
+
+<body>
+    <h1 style="color: black; font-size: 50px; text-align: center; margin: 0%; padding: 0%;">About Us</h1>
+    <div style=" width: 100%; height: 800px;">
+        <div class="col" style="width: 100%; height: 400px;">
+            <div style=" width: 66.66%; height: 500px;">
+                <img src="./img/about.jpeg" style=" width: 80%; height: 500px; padding-left: 10%; padding-right: 10%;">
+            </div>
+            <div style="width:33.33%; height: 500px;">
+                <p style="padding-left: 10%;padding-right: 10%;font-size: xx-large;">Welcome to our online app store
+                    called "Appify". Our platform offers a wide range of apps, games, movies, and ebooks for users to
+                    explore and enjoy. Whether you're looking for the latest mobile apps, popular games, blockbuster
+                    movies, or captivating ebooks, our app store has something for everyone.
+                </p>
+            </div>
+        </div>
+        <div style=" width: 100%; height: 150px;"></div>
+        <div class="col" style="width: 100%; height: 300px;">
+
+            <div class="col" style=" width: 33.3%; height: 300px;">
+
+                <div style=" width: 50%; height: 300px;">
+                    <img src="./Img/aaa.jpeg" style=" width: 100%; height: 300px; ">
+                </div>
+                <div style=" width: 50%; height: 300px;">
+                    <h1 class="hname">Chris Evans</h1><br>
+                    <p class="position" style="font-size: xx-large;">text</p> <br>
+                    <p class="descri">Our team of dedicated developers at the App Store is committed to providing an
+                        exceptional platform for creators to showcase their innovations. </p>
+
+                </div>
+
+            </div>
+            <div class="col" style="width: 33.3%; height: 300px; ">
+                <div style="width: 50%; height: 300px;">
+                    <img src="./Img/aaa.jpeg" style=" width: 100%; height: 300px; ">
+                </div>
+                <div style="width: 50%; height: 300px;">
+                    <h1 class="hname">Chris Evans</h1><br>
+                    <p class="position" style="font-size: xx-large;">text</p> <br>
+                    <p class="descri">Our team of dedicated developers at the App Store is committed to providing an
+                        exceptional platform for creators to showcase their innovations. </p>
+                </div>
+            </div>
+
+            <div class="col" style=" width: 33.3%; height: 300px;">
+                <div style="width: 50%; height: 300px;">
+                    <img src="./Img/aaa.jpeg" style=" width: 100%; height: 300px; ">
+                </div>
+                <div style="width: 50%; height: 300px;">
+                    <h1 class="hname">Chris Evans</h1><br>
+                    <p class="position" style="font-size: xx-large;">text</p> <br>
+                    <p class="descri">Our team of dedicated developers at the App Store is committed to providing an
+                        exceptional platform for creators to showcase their innovations. </p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
+
+</html>
