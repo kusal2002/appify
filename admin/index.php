8a024ba3274fb2bc596e724520ba333f657a3a30
lithiix <lithiraransika12@gmail.com>
2024-05-01 15:10:07 +0530

    manage

--- admin/index.php
+++ admin/index.php
@@ -5,118 +5,7 @@
 <head>
   <meta charset="utf-8">
   <title>Dashboard</title>
-  <style>
-    body {
-      font-family: Arial, Helvetica, sans-serif;
-    }
-
-    #dashboard {
-      background-color: lightblue;
-      padding: 10px;
-    }
-
-    .sign-out {
-      background-color: red;
-      color: white;
-      padding: 10px 20px;
-      text-align: center;
-      text-decoration: none;
-      display: inline-block;
-      font-size: 16px;
-    }
-
-    .users{
-      background-color: rgb(255, 255, 255);
-      width: 100%;
-      height: 300px;
-
-    }
-
-    .hederuser{
-      display: flex;
-      justify-content: space-between;
-    }
-
-    .apps{
-      background-color: rgb(255, 255, 255);
-      width: 100%;
-      height: 300px;
-    }
-
-  
-
-    
-    table {
-      border-collapse: collapse;
-      width: 100%;
-      text-align: left;
-    }
-    
-    h1{
-      margin: 0;
-    }
-    /* Button 99 */
-    .button {
-      top: 348px; 
-      left: 1300px; 
-      height: 36px; 
-      padding: 0 12px; 
-      font-family: Inter; 
-      font-size: 14px; 
-      line-height: 22px; 
-      font-weight: 400; 
-      color: #00BDD6FF; /* primary-500 */
-      background: #EBFDFFFF; /* primary-100 */
-      opacity: 1; 
-      border: none; 
-      border-radius: 4px; /* border-m */
-      
-    }
-    /* Hover */
-    .button:hover {
-      color: #00BDD6FF; /* primary-500 */
-      background: #A6F5FFFF; /* primary-200 */
-    }
-    /* Pressed */
-    .button:hover:active {
-      color: #00BDD6FF; /* primary-500 */
-      background: #61EDFFFF; /* primary-300 */
-    }
-    /* Disabled */
-    .button:disabled {
-      opacity: 0.4; 
-    }
-
-    .avtivebtn {
-      
-      top: 348px; 
-      left: 1300px; 
-      height: 20px; 
-      display: flex; 
-      align-items: center; 
-      justify-content: center; 
-      font-family: Inter; 
-      font-size: 20px; 
-      line-height: 22px; 
-      font-weight: 400; 
-      color: rgb(63, 183, 107); /* primary-500 */
-      background: #EBFDFFFF; /* primary-100 */
-      opacity: 1; 
-      border: none; 
-      border-radius: 4px; /* border-m */
-    }
-    /* Hover */
-    .avtivebtn:hover {
-      color: rgb(255, 255, 255); /* primary-500 */
-      background: rgb(63, 183, 107); /* primary-200 */
-    }
-    /* create */
-    .iconpen {
-      
-      
-      fill: #9095A0FF; /* neutral-500 */
-    }
-  </style>
+  <link rel="stylesheet" href="/css/admin_style.css">
 </head>
 
 <body>
@@ -129,7 +18,9 @@
     <div class="users">
       <div class="hederuser">
         <h1>Users</h1>
-        <button class="button">See More</button>
+        <a href="/publisher/user_manage.html">
+          <button class="button">See More</button>
+        </a>
       </div>
         <br>
         <table>
@@ -191,11 +82,203 @@
           </tr>
         </table>
     </div>
-    <br><br>
+    <br>
     <div class="apps">
       <div class="hederuser">
         <h1>Apps</h1>
-        <button class="button">See More</button>
+        <a href="/publisher/app_manage.html">
+          <button class="button">See More</button>
+        </a>
+        
+      </div>
+        <br>
+        <table>
+          <tr style="width: 100%;">
+            <th>App Name</th>
+            <th>App Description</th>
+            <th>Update Data</th>
+            <th>Status</th>
+            <th>Edit</th>
+          </tr>
+          <tr>
+            <td>tuda</td>
+            <td>tuda@gmail.com</td>
+            <td>10/07/2023</td>
+            <td><button class="avtivebtn">Active</button></td>
+            <td><img src="/Img/icons/pen.svg" id="iconpen"></td>
+          </tr>
+          <tr>
+            <td>tuda</td>
+            <td>tuda@gmail.com</td>
+            <td>10/07/2023</td>
+            <td><button class="avtivebtn">Active</button></td>
+            <td><img src="/Img/icons/pen.svg" id="iconpen"></td>
+          </tr>
+          <tr>
+            <td>tuda</td>
+            <td>tuda@gmail.com</td>
+            <td>10/07/2023</td>
+            <td><button class="avtivebtn">Active</button></td>
+            <td><img src="/Img/icons/pen.svg" id="iconpen"></td>
+          </tr>
+          <tr>
+            <td>tuda</td>
+            <td>tuda@gmail.com</td>
+            <td>10/07/2023</td>
+            <td><button class="avtivebtn">Active</button></td>
+            <td><img src="/Img/icons/pen.svg" id="iconpen"></td>
+          </tr>
+          <tr>
+            <td>tuda</td>
+            <td>tuda@gmail.com</td>
+            <td>10/07/2023</td>
+            <td><button class="avtivebtn">Active</button></td>
+            <td><img src="/Img/icons/pen.svg" id="iconpen"></td>
+          </tr>
+          <tr>
+            <td>tuda</td>
+            <td>tuda@gmail.com</td>
+            <td>10/07/2023</td>
+            <td><button class="avtivebtn">Active</button></td>
+            <td><img src="/Img/icons/pen.svg" id="iconpen"></td>
+          </tr>
+          
+        </table>
+
+    </div>
+    <br>
+    <div class="books">
+      <div class="hederuser">
+        <h1>Books</h1>
+        <a href="/publisher/books_manage.html">
+          <button class="button">See More</button>
+        </a>
+      </div>
+        <br>
+        <table>
+          <tr style="width: 100%;">
+            <th>App Name</th>
+            <th>App Description</th>
+            <th>Update Data</th>
+            <th>Status</th>
+            <th>Edit</th>
+          </tr>
+          <tr>
+            <td>tuda</td>
+            <td>tuda@gmail.com</td>
+            <td>10/07/2023</td>
+            <td><button class="avtivebtn">Active</button></td>
+            <td><img src="/Img/icons/pen.svg" id="iconpen"></td>
+          </tr>
+          <tr>
+            <td>tuda</td>
+            <td>tuda@gmail.com</td>
+            <td>10/07/2023</td>
+            <td><button class="avtivebtn">Active</button></td>
+            <td><img src="/Img/icons/pen.svg" id="iconpen"></td>
+          </tr>
+          <tr>
+            <td>tuda</td>
+            <td>tuda@gmail.com</td>
+            <td>10/07/2023</td>
+            <td><button class="avtivebtn">Active</button></td>
+            <td><img src="/Img/icons/pen.svg" id="iconpen"></td>
+          </tr>
+          <tr>
+            <td>tuda</td>
+            <td>tuda@gmail.com</td>
+            <td>10/07/2023</td>
+            <td><button class="avtivebtn">Active</button></td>
+            <td><img src="/Img/icons/pen.svg" id="iconpen"></td>
+          </tr>
+          <tr>
+            <td>tuda</td>
+            <td>tuda@gmail.com</td>
+            <td>10/07/2023</td>
+            <td><button class="avtivebtn">Active</button></td>
+            <td><img src="/Img/icons/pen.svg" id="iconpen"></td>
+          </tr>
+          <tr>
+            <td>tuda</td>
+            <td>tuda@gmail.com</td>
+            <td>10/07/2023</td>
+            <td><button class="avtivebtn">Active</button></td>
+            <td><img src="/Img/icons/pen.svg" id="iconpen"></td>
+          </tr>
+          
+        </table>
+
+    </div>
+    <br>
+    <div class="movies">
+      <div class="hederuser">
+        <h1>Movies</h1>
+        <a href="/publisher/books_manage.html">
+          <button class="button">See More</button>
+        </a>
+      </div>
+        <br>
+        <table>
+          <tr style="width: 100%;">
+            <th>App Name</th>
+            <th>App Description</th>
+            <th>Update Data</th>
+            <th>Status</th>
+            <th>Edit</th>
+          </tr>
+          <tr>
+            <td>tuda</td>
+            <td>tuda@gmail.com</td>
+            <td>10/07/2023</td>
+            <td><button class="avtivebtn">Active</button></td>
+            <td><img src="/Img/icons/pen.svg" id="iconpen"></td>
+          </tr>
+          <tr>
+            <td>tuda</td>
+            <td>tuda@gmail.com</td>
+            <td>10/07/2023</td>
+            <td><button class="avtivebtn">Active</button></td>
+            <td><img src="/Img/icons/pen.svg" id="iconpen"></td>
+          </tr>
+          <tr>
+            <td>tuda</td>
+            <td>tuda@gmail.com</td>
+            <td>10/07/2023</td>
+            <td><button class="avtivebtn">Active</button></td>
+            <td><img src="/Img/icons/pen.svg" id="iconpen"></td>
+          </tr>
+          <tr>
+            <td>tuda</td>
+            <td>tuda@gmail.com</td>
+            <td>10/07/2023</td>
+            <td><button class="avtivebtn">Active</button></td>
+            <td><img src="/Img/icons/pen.svg" id="iconpen"></td>
+          </tr>
+          <tr>
+            <td>tuda</td>
+            <td>tuda@gmail.com</td>
+            <td>10/07/2023</td>
+            <td><button class="avtivebtn">Active</button></td>
+            <td><img src="/Img/icons/pen.svg" id="iconpen"></td>
+          </tr>
+          <tr>
+            <td>tuda</td>
+            <td>tuda@gmail.com</td>
+            <td>10/07/2023</td>
+            <td><button class="avtivebtn">Active</button></td>
+            <td><img src="/Img/icons/pen.svg" id="iconpen"></td>
+          </tr>
+          
+        </table>
+
+    </div>
+    <br>
+    <div class="blogs">
+      <div class="hederuser">
+        <h1>Blogs</h1>
+        <a href="/blogger/blog_manage.html">
+          <button class="button">See More</button>
+        </a>
       </div>
         <br>
         <table>
---
+++ blogger/layout/footer.php
@@ -0,0 +1,45 @@
+<div class="footer">
+    <div class="container">
+        <div class="logo">
+            <img src="img/logo.png" alt="logo">
+        </div>
+        <div class="links">
+            <table>
+                <tr>
+                    <td>
+                        <h2>Home</h2>
+                    </td>
+                    <td>
+                        <h2>Product & Service</h2>
+                    </td>
+                    <td>
+                        <h2>Policies</h2>
+                    </td>
+                </tr>
+                <tr>
+                    <td><a href="#">Blog</a></td>
+                    <td><a href="#">Publish Your Blog</a></td>
+                    <td><a href="#">Privacy Policy</a></td>
+                </tr>
+                <tr>
+                    <td><a href="#">Feedback</a></td>
+                    <td><a href="#">Publish Your App</a></td>
+                    <td><a href="#">Terms & Conditions</a></td>
+                </tr>
+                <tr>
+                    <td><a href="#">Feedback</a></td>
+                    <td><a href="#">Publish Movies</a></td>
+                </tr>
+                <tr>
+                    <td><a href="#">FAQ</a></td>
+                </tr>
+
+            </table>
+
+        </div>
+
+    </div>
+    <div class="cren">
+        <h4>© 2024 Sliit MLB_07.02_09.</h4>
+    </div>
+</div>
