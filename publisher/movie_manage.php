89029560f0ad79ea81079b9745baf7f3c9c839b3
Kusal Salpura <kusalsalpura@gmail.com>
2024-05-01 13:56:58 +0530

    fixed

--- publisher/movie_manage.php
+++ publisher/movie_manage.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8">
     <title>Manage Movies</title>
-    <link rel="stylesheet" href="/css/style.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> -->
 </head>
 
 <body>
--- publisher/user_manage.php
+++ publisher/user_manage.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <title>Manage Users</title>
-    <link rel="stylesheet" href="/css/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
