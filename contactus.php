997217ac52e86d908adbdee5f40778700c4d039e
Tudakshana <tudakshanamjayawardhana@gmail.com>
2024-05-05 23:15:12 +0530

    a

--- contactus.php
+++ contactus.php
@@ -6,46 +6,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Us</title>
     <link rel="stylesheet" href="./css/contactus.css">
+    <link rel="stylesheet" href="./css/style.css"> 
 </head>
 
 <body>
-    <div class="space"></div>
+<?php include_once './layout/header.php' ?>
+
+    
     <div class="full">
         
         <div class="left">
             
             <h1 class="head">Contact Us</h1><br>
-            <p class="info">
-                <img src="./Img/locationtag.svg">
-                1777 West Street, Portland, OR 97205
+            <div class="info">
+                <img class="svg" src="./Img/location.svg">
+                <label id="det">777 West Street, Portland, OR 97205</label>
                 <br><br>
-                <img src="./Img/telephone.svg">
-                (+1) 123 456 7893
+                <img class="svg" src="./Img/tele.svg">
+                <label id="det">(+1) 123 456 7893</label>
                 <br><br>
-                <img src="./Img/mail.svg">
-                name@email.com
-
-            </p>
+                <img class="svg" src="./Img/mail.svg">
+                <label id="det">name@email.com</label>
+                <br>
+            </div>
             <img src="./Img/image.jpeg" class="image">
 
         </div>
         <div class="line"></div>
         <div class="right">
             <form action="" class="form">
-                <lable>Name</lable><br>
-                <input type=text size="60"><br>
-                <lable>Email</lable><br>
-                <input type=text size="60"><br>
-                <lable>Contact No</lable><br>
-                <input type=text size="60"><br><br>
-                <lable>Your Message</lable><br>
+                <label>Name</label><br>
+                <input type=text size="72"><br><br>
+                <label>Email</label><br>
+                <input type=text size="72"><br><br>
+                <label>Contact No</label><br>
+                <input type=text size="72"><br><br>
+                <label>Your Message</label><br>
                 <textarea cols="60" rows="10"></textarea><br><br>
-                <button>Send Message</button>
+                <button onclick="alertbox()">Send Message</button>
             </form>
 
         </div>
+    </div>
+ <?php include_once './layout/footer.php' ?>
 
-
+ <script>
+    function alertbox(){
+        alert('Successfully Sent');
+    }
+ </script>
 </body>
+
 
 </html>
