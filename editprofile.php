a98adc96ae99aef372377eb1ac198f1ae69dadd2
Tudakshana <tudakshanamjayawardhana@gmail.com>
2024-05-07 04:32:43 +0530

    a

--- editprofile.php
+++ editprofile.php
@@ -14,7 +14,7 @@ if (!empty($_SESSION['sessionid'])) {
 
 $id = $row['user_id'];
 
-$sql="select * from `user` where user_id=$id";
+$sql="SELECT * FROM `user` WHERE user_id=$id";
 $result = mysqli_query($conn, $sql);
 $row=mysqli_fetch_assoc($result);
 $fname=$row['f_name'];
@@ -29,7 +29,7 @@ if(isset($_POST['submit'])) {
     $email = $_POST['email'];
     $mobile = $_POST['mobile'];
 
-    $sql="update `user` set f_name='$fname',l_name='$lname',email='$email',phone_number='$mobile' where id=$id";
+    $sql="UPDATE `user` SET f_name='$fname',l_name='$lname',email='$email',phone_number='$mobile' where id=$id";
     $result = mysqli_query($conn, $sql);
     if($result) {
         echo '<script>alert("Record Updated Successfully")</script>';
@@ -51,30 +51,33 @@ if(isset($_POST['submit'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit Profile</title>
     <link rel="stylesheet" href="./css/editprofile_style.css">
+    
 </head>
 <body>
-        <div class="col" style="background-color: blue; width: 100%; height: 100%; padding:0; margin: 0;"> 
-            <div style="background-color: rgb(255, 255, 255); width: 30%; height: 700px;">
+        <div class="col" style="background-color:  #2d3c3e; height: 100%; padding:0; margin: 0;"> 
+            <div style="background-color: rgb(255, 255, 255); width: 30%; height: 800px;">
             <img src="./Img/pro.png" style="width: 200px;height: 200px; padding-left: 25%;padding-top: 20%; padding-right: 25%;">
             
         </div>
-            <div style="background-color: rgb(255, 255, 255);; width: 70%; height: 700px; ">
+            <div style="background-color:  #2d3c3e; width: 70%; height: 600px; ">
             <form style="padding-top: 5%; padding-left: 10%;" action="">
 
-                <label for="fullname">First Name</label><br>
+                <label for="fullname" style="color: white;">First Name</label><br>
                 <input class="text" type="text" name="fname" value="<?php echo $fname; ?>"><br><br>
-                <label for="fullname">Last Name</label><br>
+                <label for="fullname" style="color: white;" >Last Name</label><br>
                 <input class="text"  name="lname" type="text" value="<?php echo $lname; ?>"><br><br>
-                <label for="email">Email</label><br>
+                <label for="email" style="color: white">Email</label><br>
                 <input class="text" type="text"name="email" value="<?php echo $email; ?>"><br><br>
-                <label for="PhoneNumber">Phone Number</label><br>
+                <label for="PhoneNumber" style="color: white;">Phone Number</label><br>
                 <input class="text" type="text"name="mobile" value="<?php echo $mobile; ?>"><br><br>
+
+                <button class="btn" style="margin-right: 8%;" type="submit" name="submit">Save Profile</button>
+                
                 
                 
             </form>
             
-            <button class="btn" style="margin-right: 8%;">Save Profile</button>
-            <button class="btn">Cancel</button>
+            
             </div>            
         </div>
 
